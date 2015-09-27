ebaa4ed5776ad86779bb445000b947952bff949b
brainaconly <maxonly9@gmail.com>
2015-09-26 21:11:59 -0700

    First application run with a fake data

--- SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Web.Mvc;
 using Ninject;
+using System.Linq;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Infrastructure
 {
@@ -27,7 +31,14 @@ namespace SportsStore.WebUI.Infrastructure
 
         public void AddBindings()
         {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product> {
+                new Product { Name = "Football", Price = 25},
+                new Product { Name = "Surf board", Price = 179},
+                new Product { Name = "Running shoes", Price = 95}
+            });
 
+            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
         }
     }
 }
