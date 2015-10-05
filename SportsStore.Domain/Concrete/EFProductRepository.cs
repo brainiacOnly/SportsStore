988785624a14ceba5937f45f29b70515429395f6
brainaconly <maxonly9@gmail.com>
2015-10-04 22:27:05 -0700

    CRUD for SportsStore products

--- SportsStore.Domain/Concrete/EFProductRepository.cs
+++ SportsStore.Domain/Concrete/EFProductRepository.cs
@@ -1,6 +1,7 @@
 ﻿using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
 using System.Collections.Generic;
+using System.Data.Entity;
 
 namespace SportsStore.Domain.Concrete
 {
@@ -8,12 +9,48 @@ namespace SportsStore.Domain.Concrete
     {
         private EFDbContext context = new EFDbContext();
 
+        public EFProductRepository()
+        {
+            Database.SetInitializer<EFDbContext>(null);
+        }
+
         public IEnumerable<Product> Products
         {
             get
             {
                 return context.Products;
             }
+        }
+
+        public void SaveProduct(Product product)
+        {
+            if (product.ProductID == 0)
+            {
+                context.Products.Add(product);
+            }
+            else
+            {
+                var dbEntry = context.Products.Find(product.ProductID);
+                if (dbEntry != null)
+                {
+                    dbEntry.Name = product.Name;
+                    dbEntry.Description = product.Description;
+                    dbEntry.Price = product.Price;
+                    dbEntry.Category = product.Category;
+                }
+            }
+            context.SaveChanges();
+        }
+
+        public Product DeleteProduct(int productID)
+        {
+            var dbEntry = context.Products.Find(productID);
+            if (dbEntry != null)
+            {
+                context.Products.Remove(dbEntry);
+                context.SaveChanges();
+            }
+            return dbEntry;
         }
     }
 }
---
+++ SportsStore.UnitTests/AdminTests.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class AdminTests
+    {
+        [TestMethod]
+        public void IndexContainsAllProducts()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new[]
+            {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"}
+            });
+            var target = new AdminController(mock.Object);
+
+            var result = ((IEnumerable<Product>) target.Index().ViewData.Model).ToArray();
+
+            Assert.AreEqual(result.Length, 3);
+            Assert.AreEqual("P1", result[0].Name);
+            Assert.AreEqual("P2", result[1].Name);
+            Assert.AreEqual("P3", result[2].Name);
+        }
+
+        [TestMethod]
+        public void CanEditProduct()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new[]
+            {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"}
+            });
+            var target = new AdminController(mock.Object);
+
+            var p1 = target.Edit(1).ViewData.Model as Product;
+            var p2 = target.Edit(2).ViewData.Model as Product;
+            var p3 = target.Edit(3).ViewData.Model as Product;
+
+            Assert.AreEqual(1, p1.ProductID);
+            Assert.AreEqual(2, p2.ProductID);
+            Assert.AreEqual(3, p3.ProductID);
+        }
+
+        [TestMethod]
+        public void CannotEditNonExistentProduct()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new[]
+            {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"}
+            });
+            var target = new AdminController(mock.Object);
+
+            var result = target.Edit(4).ViewData.Model as Product;
+
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void CanSaveValidCahges()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            AdminController target = new AdminController(mock.Object);
+            Product product = new Product {Name = "Test"};
+
+            ActionResult result = target.Edit(product);
+
+            mock.Verify(m => m.SaveProduct(product));
+            Assert.IsNotInstanceOfType(result, typeof (ViewResult));
+        }
+
+        [TestMethod]
+        public void CannotSaveInvalidChanges()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            AdminController target = new AdminController(mock.Object);
+            Product product = new Product {Name = "Test"};
+
+            target.ModelState.AddModelError("error", "error");
+            ActionResult result = target.Edit(product);
+
+            mock.Verify(m => m.SaveProduct(It.IsAny<Product>()), Times.Never());
+            Assert.IsInstanceOfType(result, typeof (ViewResult));
+        }
+
+        [TestMethod]
+        public void CanDeleteValidProducts()
+        {
+            var prod = new Product {ProductID = 2, Name = "Test"};
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new[]
+            {
+                new Product {ProductID = 1, Name = "P1"},
+                prod,
+                new Product {ProductID = 3, Name = "P3"}
+            });
+            var target = new AdminController(mock.Object);
+
+            target.Delete(prod.ProductID);
+
+            mock.Verify(m => m.DeleteProduct(prod.ProductID));
+        }
+    }
+}
